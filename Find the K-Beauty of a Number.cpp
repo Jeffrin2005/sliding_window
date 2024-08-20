2c463dd2ab8f92e674fa6016bfd4c92a5741f78f
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-08-20 13:10:41 +0530

    Update Find the K-Beauty of a Number.cpp

--- Find the K-Beauty of a Number.cpp
+++ Find the K-Beauty of a Number.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     int divisorSubstrings(int num, int k) {
+        string str = to_string(num);
+        int i = 0, j = 0, n = str.size();
+        int ans = 0;
+        while(j < n){
+            if(j - i + 1 < k){
+                j++;
+            }
+            else if(j - i + 1 == k){
+                string s = str.substr(i,k);
+                int n = stoi(s);
+                if(n != 0 && num % n == 0 ){
+                    ans++;
+                }
+                ++i;
+                ++j;
+            }
+                
+        }
+        
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int divisorSubstrings(int num, int k) {
          std::string numStr = std::to_string(num);
     int count = 0;
     for (int i = 0; i <= numStr.size() - k; ++i) {
