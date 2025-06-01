c4673d710db4339a7bc2406173d6633f6031ba16
jevinjojo <jevinjojo1@gmail.com>
2025-06-01 12:32:49 +0530

    github-user

--- Find the Power of K-Size Subarrays I.cpp
+++ Find the Power of K-Size Subarrays I.cpp
@@ -16,6 +16,8 @@ public:// time comp = o(n), space comp = o(1);
             while (i < j && j - i + 1 > k) {
                 i++;
             }
+
+            // ADDi take and tak ei sdfskdf sldfo.e asdhello wrold in int si pron
             // finall submission ... 
 
             // If the window size is exactly k, set the power
